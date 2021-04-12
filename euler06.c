6a0595d1eb2f58275e7d001db8c82db48cac0498
mtowerss <69701161+mtowerss@users.noreply.github.com>
2021-04-12 10:15:19 -0400

    Create euler06.c

---
+++ euler06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int sumSquares = 0;
+  int sqaureSum = 0;
+  int finalSum = 0;
+  int sum = 0;
+  for(int i = 0; i <= 100; i++){ 
+    sum = (i*i);
+    sumSquares += sum
+    squareSum += i;
+  }
+  finalSum = (squareSum*squareSum) - sumSquares;
+  printf("Total difference is %d, finalSum);
+  return 0;
+}
+	 
