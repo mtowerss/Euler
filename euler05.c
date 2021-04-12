016fd3d3cb8d2710ceec90f2c948283ebe58538e
mtowerss <69701161+mtowerss@users.noreply.github.com>
2021-04-12 10:14:51 -0400

    Create euler05.c

---
+++ euler05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int smallest = 1;
+  int counter = smallest;
+  
+  for(int x = 2; x < 20; x++){
+    while(counter % x != 0){
+      counter += smallest;
+    }
+    smallest = counter
+  }
+
+  printf("Smallest positive number divisible by 1 to 20 %d", smallest
+} 
